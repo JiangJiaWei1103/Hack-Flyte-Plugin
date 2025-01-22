2c1a3f8976e243c0c540481a490305a7750cf7f5
JiangJiaWei1103 <waynechuang97@gmail.com>
2025-01-22 22:05:46 +0800

    Support configurable db qeury
    Signed-off-by: JiangJiaWei1103 <waynechuang97@gmail.com>

--- examples/tiny_duckdb.py
+++ examples/tiny_duckdb.py
@@ -6,6 +6,8 @@ Parent task: PythonInstanceTask
 ---
 
 ## Some tmp notes
+* Not an agent
+    * No extra executor is used, just overriding execute method
 """
 import os
 from typing_extensions import Annotated
@@ -20,27 +22,37 @@ from flytekitplugins.duckdb import DuckDBQuery
 def _gen_df() -> pd.DataFrame:
     return pd.DataFrame({
         "name": ["John", "Sam"],
-        "height": [171, 190]
+        "height": [171, 190],
+        "salary": [100, 90],
     })
 
 
 s_all_query = DuckDBQuery(
     name="s-all",
-    query="SELECT * from df",
+    query="SELECT * FROM df",
     inputs=kwtypes(df=pd.DataFrame)
 )
 
 
+flex_query = DuckDBQuery(
+    name="flex",
+    inputs=kwtypes(df=pd.DataFrame, query=str)
+)
+
+
 @workflow
-def pd_wf() -> pd.DataFrame:
+def pd_wf(query: str) -> pd.DataFrame:
     df = _gen_df()
-    return s_all_query(df=df)
+    res = s_all_query(df=df)
+    res = flex_query(df=res, query=query)
+
+    return res
 
 
 if __name__ == "__main__":
     print(f"Test DuckDBQuery...\n")
 
-    # Set query here ...
+    s_sum = "SELECT SUM(salary) FROM df"
 
     print(">>> QUERY RESULT <<<")
-    print(pd_wf())
+    print(pd_wf(query=s_sum))
